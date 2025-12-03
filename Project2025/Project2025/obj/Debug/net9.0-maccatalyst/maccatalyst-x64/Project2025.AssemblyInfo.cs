db528eaa9a6b8596e339ceab4665e1de1a223e90
Katya_Mez <katya201206@gmail.com>
2025-12-03 21:23:32 +0000

    Empty Project

--- Project2025/Project2025/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Project2025.AssemblyInfo.cs
+++ Project2025/Project2025/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/Project2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3434c2d1970d66af0b3ac974fb905452a516b31e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project2025/Project2025/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Project2025.AssemblyInfo.cs
+++ Project2025/Project2025/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Project2025.AssemblyInfo.cs
@@ -11,10 +11,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.project2025")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "Project2025")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3434c2d1970d66af0b3ac974fb905452a516b31e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
